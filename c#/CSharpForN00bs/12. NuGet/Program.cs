7ccc6daa1f1b9fa963e2c2eb322afcbbcd33f206
Muhammad Aprian <n3wbye@protonmail.com>
2023-09-10 12:17:34 +0700

    Add 'NuGet in C# (CSharpForN00bs)'

---
+++ c#/CSharpForN00bs/12. NuGet/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+using RestSharp;
+
+var client = new RestClient(new Uri("https://api.coingecko.com/api/v3/"));
+var req = new RestRequest("simple/price?ids=bitcoin&vs_currencies=GBP%2CUSD");
+var resp = await client.GetAsync<Root>(req);
+
+Console.WriteLine("Current BTC Price:");
+Console.WriteLine($"USD: £{resp.Bitcoin.Usd}");
+Console.WriteLine($"GBP: ${resp.Bitcoin.Gbp}");
+internal class Bitcoin
+{
+    [JsonPropertyName("gbp")] public double Gbp { get; set; }
+    [JsonPropertyName("usd")] public double Usd { get; set; }
+}
+
+internal class Root
+{
+    [JsonPropertyName("bitcoin")] public Bitcoin Bitcoin { get; set; }
+}
