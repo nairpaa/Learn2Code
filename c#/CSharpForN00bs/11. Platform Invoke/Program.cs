9b4021efcfdb2d0451fb0ae0d22018132fcb12c9
Muhammad Aprian <n3wbye@protonmail.com>
2023-09-10 11:49:24 +0700

    Add 'Platform Invoke in C# (CSharpForN00bs)'

---
+++ c#/CSharpForN00bs/11. Platform Invoke/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+[DllImport("kernel32", SetLastError = true)]
+static extern nint OpenProcess(uint dwDesiredAccess, bool bInheritHandle, uint dwProcessId);
+
+var hProcess = OpenProcess(0xF01FF, false, 26768);
+
+if (hProcess == nint.Zero)
+    Console.WriteLine("OpenProcess failed {0}", Marshal.GetLastWin32Error());
+else
+    Console.WriteLine("hProcess: 0x{0}", hProcess);
